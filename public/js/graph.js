b54f3eacae28281fc348645de9e30e94f9df4af7
Shey <sheharyar.u@gmail.com>
2019-12-03 11:58:32 -0500

    labeled axis for bar chart

--- public/js/graph.js
+++ public/js/graph.js
@@ -21,57 +21,83 @@ var generateData = () => {
 var generateGraph = () => {
   let data = generateData()
 
+  const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', 700)
+    .attr('height', 600)
+
+  // Create margins and dimensions
+  const margin = {
+    top: 20,
+    right: 20,
+    bottom: 100,
+    left: 100
+  }
+  const graphWidth = 700 - margin.left - margin.right
+  const graphHeight = (600 - margin.top - margin.bottom)
+
+  const graph = svg.append('g')
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+  const xAxisGroup = graph.append('g')
+    .attr('transform', `translate(0, ${graphHeight})`)
+  const yAxisGroup = graph.append('g')
+
+
+  const rects = graph.selectAll('rect')
+    .data(data)
+
   let xDomain = [];
   for (let i = 0; i < data.length; i++) {
-    xDomain.push(Object.keys(data[i])[0]);
+    xDomain.push((Object.keys(data[i])[0]));
   }
 
-  console.log(xDomain);
-
-  let maxY = 0;
-  for (let i = 0; i < data.length; i++) {
-    if (parseInt((Object.values(data[i])[0])) > maxY) {
-      maxY = (Object.values(data[i])[0])
-    }
-  }
-
-  console.log(maxY);
-
-  const svg = d3.select('svg');
+  const min = d3.min(data, d => Number(Object.values(d)[0]))
+  const max = d3.max(data, d => Number(Object.values(d)[0]))
 
   const y = d3.scaleLinear()
-    .domain([0, maxY * 1.4])
-    .range([0, 500]);
+    .domain([0, d3.max(data, d => Number(Object.values(d)[0]))])
+    .range([graphHeight, 0]);
 
   const x = d3.scaleBand()
     .domain(xDomain)
-    .range([0, 600])
+    .range([0, graphWidth])
     .paddingInner(0.05)
     .paddingOuter(0.2)
 
-
-  const rects = svg.selectAll('rect')
-    .data(data)
-
-
   rects.attr('width', x.bandwidth)
     .attr('height', (d, i) => {
-      return y(d[xDomain[i]])
+      return graphHeight - y(d[xDomain[i]])
     })
     .attr('fill', 'teal')
     .attr('x', d => x((Object.keys(d)[0])))
+    .attr('y', (d, i) => y(d[xDomain[i]]))
 
   rects.enter()
     .append('rect')
     .attr('width', x.bandwidth)
     .attr('height', (d, i) => {
-      return y(d[xDomain[i]])
+      return graphHeight - y(d[xDomain[i]])
     })
     .attr('fill', 'teal')
     .attr('x', d => x((Object.keys(d)[0])))
+    .attr('y', (d, i) => y(d[xDomain[i]]))
+
+  // Create and call axis
+  const xAxis = d3.axisBottom(x)
+  const yAxis = d3.axisLeft(y)
+
+
+  xAxisGroup.call(xAxis)
+  yAxisGroup.call(yAxis)
 
 }
 generateGraph()
+
+
+
 
 submitFields.addEventListener('click', () => {
   let arr = [];
