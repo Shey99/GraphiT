ca9a69f55ef44ddd52c0c8ade97f0b4efe914ba5
Shey <sheharyar.u@gmail.com>
2019-12-09 14:57:21 -0500

    cleaned up code - seperate generate data function and update graph function

--- public/js/bar.js
+++ public/js/bar.js
@@ -5,7 +5,88 @@ const graphName = document.querySelector('.graphName').innerHTML;
 const cancel = document.querySelectorAll('.cancel');
 const download = document.querySelector('.download');
 
-var generateData = () => {
+
+// Select SVG container
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', 700)
+  .attr('height', 600)
+
+// Create margins and dimensions
+const margin = { top: 20, right: 20, bottom: 100, left: 100 }
+const graphWidth = 700 - margin.left - margin.right
+const graphHeight = (600 - margin.top - margin.bottom)
+
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+// Scales
+const y = d3.scaleLinear()
+  .range([graphHeight, 0]);
+
+const x = d3.scaleBand()
+  .range([0, graphWidth])
+  .paddingInner(0.05)
+  .paddingOuter(0.2)
+
+// Create axes group
+const xAxisGroup = graph.append('g').attr('transform', `translate(0, ${graphHeight})`)
+const yAxisGroup = graph.append('g')
+
+// Create and call axis
+const xAxis = d3.axisBottom(x)
+const yAxis = d3.axisLeft(y)
+
+// Update axes text
+xAxisGroup.selectAll('text')
+  .attr('transform', 'rotate(-40)')
+  .attr('text-anchor', 'end')
+  .attr('fill', 'white')
+yAxisGroup.selectAll('text')
+  .attr('fill', 'white')
+
+// Update Function
+const update = (data) => {
+
+  // Update scale domains
+  const xDomain = data.map(item => Object.keys(item)[0])
+  const max = d3.max(data, d => Number(Object.values(d)[0]))
+
+  y.domain([0, d3.max(data, d => Number(Object.values(d)[0]))])
+  x.domain(xDomain)
+
+  // Join data to rects
+  const rects = graph.selectAll('rect')
+    .data(data)
+
+  // Remove Exit Selection
+  rects.exit().remove()
+
+  // Update Current Selection In DOM
+  rects.attr('width', x.bandwidth)
+    .attr('height', (d, i) => { return graphHeight - y(d[xDomain[i]])})
+    .attr('fill', 'teal')
+    .attr('x', d => x((Object.keys(d)[0])))
+    .attr('y', (d, i) => y(d[xDomain[i]]))
+
+  // Append the enter selection to DOM
+  rects.enter()
+    .append('rect')
+    .attr('width', x.bandwidth)
+    .attr('height', (d, i) => {return graphHeight - y(d[xDomain[i]])})
+    .attr('fill', 'teal')
+    .attr('x', d => x((Object.keys(d)[0])))
+    .attr('y', (d, i) => y(d[xDomain[i]]))
+
+  // Call Axes
+  xAxisGroup.call(xAxis)
+  yAxisGroup.call(yAxis)
+
+}
+
+const generateData = () => {
   let data = [];
   let inputs = document.querySelectorAll('.input');
   let value = document.querySelectorAll('.value');
@@ -19,88 +100,9 @@ var generateData = () => {
   return data
 }
 
-var generateGraph = () => {
-  let data = generateData()
+update(generateData())
 
-  const svg = d3.select('.canvas')
-    .append('svg')
-    .attr('width', 700)
-    .attr('height', 600)
-
-  // Create margins and dimensions
-  const margin = {
-    top: 20,
-    right: 20,
-    bottom: 100,
-    left: 100
-  }
-  const graphWidth = 700 - margin.left - margin.right
-  const graphHeight = (600 - margin.top - margin.bottom)
-
-  const graph = svg.append('g')
-    .attr('width', graphWidth)
-    .attr('height', graphHeight)
-    .attr('transform', `translate(${margin.left}, ${margin.top})`)
-
-  const xAxisGroup = graph.append('g')
-    .attr('transform', `translate(0, ${graphHeight})`)
-  const yAxisGroup = graph.append('g')
-
-  const rects = graph.selectAll('rect')
-    .data(data)
-
-  let xDomain = [];
-  for (let i = 0; i < data.length; i++) {
-    xDomain.push((Object.keys(data[i])[0]));
-  }
-
-  const min = d3.min(data, d => Number(Object.values(d)[0]))
-  const max = d3.max(data, d => Number(Object.values(d)[0]))
-
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => Number(Object.values(d)[0]))])
-    .range([graphHeight, 0]);
-
-  const x = d3.scaleBand()
-    .domain(xDomain)
-    .range([0, graphWidth])
-    .paddingInner(0.05)
-    .paddingOuter(0.2)
-
-  rects.attr('width', x.bandwidth)
-    .attr('height', (d, i) => {
-      return graphHeight - y(d[xDomain[i]])
-    })
-    .attr('fill', 'teal')
-    .attr('x', d => x((Object.keys(d)[0])))
-    .attr('y', (d, i) => y(d[xDomain[i]]))
-
-  rects.enter()
-    .append('rect')
-    .attr('width', x.bandwidth)
-    .attr('height', (d, i) => {
-      return graphHeight - y(d[xDomain[i]])
-    })
-    .attr('fill', 'teal')
-    .attr('x', d => x((Object.keys(d)[0])))
-    .attr('y', (d, i) => y(d[xDomain[i]]))
-
-  // Create and call axis
-  const xAxis = d3.axisBottom(x)
-  const yAxis = d3.axisLeft(y)
-
-  xAxisGroup.call(xAxis)
-  yAxisGroup.call(yAxis)
-
-  xAxisGroup.selectAll('text')
-    .attr('transform', 'rotate(-40)')
-    .attr('text-anchor', 'end')
-    .attr('fill', 'white')
-
-  yAxisGroup.selectAll('text')
-    .attr('fill', 'white')
-}
-generateGraph()
+const table = document.querySelector('.table');
 
 submitFields.addEventListener('click', () => {
   let arr = [];
@@ -113,7 +115,6 @@ submitFields.addEventListener('click', () => {
       arr.push(obj)
     }
   }
-  console.log(arr);
   fetch('/graphs/changeGraph', {
       method: 'put',
       headers: {
